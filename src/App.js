ed8d9909dc7137b19d9c2ddcaf6891acea14ae21
marib <i.maribsultan@gmail.com>
2023-07-03 14:19:48 +0500

    added decrement button

--- src/App.js
+++ src/App.js
@@ -6,19 +6,27 @@ import './App.css';
 const App = () => {
   const [count, setCount] =  useState(0);
 
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Current count : {count}.
-        </p>
-        <button onClick={()=>{setCount(count+1)}}>Increment</button>
-        <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    );
+      <p className="App-intro">
+        Current count : {count}.
+      </p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
